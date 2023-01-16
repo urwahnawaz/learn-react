1a06d0a3b3bd64dafe347ab0e1c561816a9fc9ca
unawaz1996 <urwah.nawaz@adelaide.edu.au>
2023-01-16 11:31:18 +1100

    changes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-//ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
+// //ReactDOM.render(<h1>Hello, everyone</h1>, document.getElementById("root"))
 
-const h1 = document.createElement("h1")
-h1.textContent = "Hello world"
-h1.className = "header"
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
 
-const element = <h1 className="header"> This is JSX</h1>
+// const element = <h1 className="header"> This is JSX</h1>
 
 
-ReactDOM.render(element, document.getElementById("root"))
+// ReactDOM.render(element, document.getElementById("root"))
+
+
+const navbar = (
+    <nav>
+        <h1>Name 
+        </h1>
+        <ul>
+            <li> Price </li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
